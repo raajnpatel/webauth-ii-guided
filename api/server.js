205e72c4ee124134a58ef72105421d231fab7c6e
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-17 10:36:00 -0800

    persistence  added to login

--- api/server.js
+++ api/server.js
@@ -1,23 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const sessions = require('express-session');
+const sessions = require('express-session'); // install express-sessions
+const KnexSessionStore = require('connect-session-knex')(sessions); // to store sessions in DB
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const knex = require('../database/dbConfig');
 
 const server = express();
 
 const sessionsConfig = {
-  secret: 'Secrets', // used for encryption (must be an environtment variable
-  name: 'Cookie Monster', // default would be sid
-  cookie: {
-    maxAge:600000, // 1000 (second) * 60 (minute) * 60 (hour) TIME IN MILLISECONDS
-    secure: process.env.NODE_ENV === "production" ? true : false,
-    httpOnly: true
-  },
-  resave: false,
-  saveUninitialized: true, // true during development, false for production
+    // sessions storage options
+    secret: 'Secrets', // used for encryption (must be an environtment variable
+    name: 'Cookie Monster', // default would be sid
+    resave: false,
+    saveUninitialized: true, // true during development, false for production
+
+    // how to store the sessions
+    store: new KnexSessionStore({
+        // DO NOT FORGET THE NEW KEYWORD
+        knex, // imported from dbConfig.js
+        createtable: true,
+        clearInterval: 60000,
+        tablename: 'sessions',
+        sidfieldname: "sid"
+    }),
+
+    // cookie options
+    cookie: {
+        maxAge:600000, // 1000 (second) * 60 (minute) * 60 (hour) TIME IN MILLISECONDS
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        httpOnly: true
+    }
 };
 
 server.use(helmet());
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,32 +1,9 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-            req.session.user = user;
-          next();
-        } else {
-          res
-              .status(401)
-              .json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-          console.log(error);
+    if (req.session && req.session.user) {
+        next();
+    } else {
         res
-            .status(500)
-            .json({ message: 'Ran into an unexpected error' });
-      });
-  } else {
-    res
-        .status(400)
-        .json({ message: 'No credentials provided' });
-  }
+            .status(401)
+            .json({ message: "Not Authorized. Please Log In." });
+    }
 };
